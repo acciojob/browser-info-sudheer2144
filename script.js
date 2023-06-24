61a3fdd72813779cdefcc649f0be86828b457d36
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2023-06-24 11:13:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+let browserDetails=navigator;
+
+let info="You are using "+browserDetails.appName+" version "+browserDetails.appVersion;
+
+document.getElementById("browser-info").innerText=info;
